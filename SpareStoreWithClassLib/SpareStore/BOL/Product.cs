9e8fb065c7bec44aba3bc1a4ae632054f5f2c0ee
Abhishek Shinde <shindeabhishek113@gmail.com>
2023-01-09 22:41:14 +0530

    New Class Lib Spare Store App

---
+++ SpareStoreWithClassLib/SpareStore/BOL/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace BOL;
+public class Product
+{
+    public int Productid{get;set;}
+    public string Name{get;set;}
+    public double Price{get;set;}
+    public int Quantity{get;set;}
+
+
+    public Product (){
+
+    }
+
+    public Product (int productid, string name, double price, int quantity){
+
+        this.Productid = productid;
+
+        this.Name = name;
+
+        this.Price = price;
+
+        this.Quantity = quantity;
+
+    }
+
+    
+
+}
---
+++ SpareStoreWithClassLib/SpareStore/SpareStoreWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SpareStoreWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
